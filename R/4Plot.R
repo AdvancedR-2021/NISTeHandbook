0cafa848449c0313324a0aaa399df891ec28c23a
LauraFolmer <201905054@post.au.dk>
2021-10-29 17:15:29 +0200

    Very simple test plots

--- R/4Plot.R
+++ R/4Plot.R
@@ -9,6 +9,30 @@
 #'
 #'
 
+library(ggplot2)
+library(tidyverse)
+
 fourPlot <- function(data) {
-  plot(range(1:len(data)),data)
+
+  data <- data
+
+  seq_plot <- data %>%
+    ggplot(mapping = aes(x = 1:length(Y), y = Y)) +
+    labs(x = "Index", y = "Transmittance") +
+    geom_point(shape = 4)
+  #print(seq_plot)
+
+  data$Ylag <- lag(data$Y,1)
+
+  lag_plot <- data %>%
+    ggplot(mapping = aes(x=Ylag, y=Y)) +
+    labs(x = expression(Y[i-1]), y=expression(Y[i])) +
+    geom_point(shape = 4)
+  #print(lag_plot)
+
+  hist_plot <- data %>%
+    ggplot(mapping = aes(x=Y)) +
+    geom_histogram()
+  print(hist_plot)
 }
+
