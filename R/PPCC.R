6fc98eb0c2a80a037acef22ac8cce159f1ce442a
LauraFolmer <201905054@post.au.dk>
2021-11-06 20:22:11 +0100

    Modified

--- R/PPCC.R
+++ R/PPCC.R
@@ -1,23 +1,29 @@
-# The Tukey-Lambda PPCC plot is used to suggest an appropriate distribution.
-# You should follow-up with PPCC and probability plots of the appropriate alternatives.
-# https://www.itl.nist.gov/div898/handbook/eda/section3/ppccplot.htm
-
-U_i <- function(ordered_data){
-  n <- length(ordered_data)
-  m_i <- rep(NA, n)
-  m_i[n] <- 0.5**(1/n)
-  m_i[1] <- 1 - m_i[n]
-  for (i in 2:n-1){
-    m_i[i] <- (i - 0.3175)/(n + 0.365)
-  }
-  m_i
-}
-
-tukey_order <- function(mi, lambda){
-  (mi**lambda - (1-mi)**lambda) / lambda
-}
-
+#' PPCC
+#'
+#' @param data The data
+#' @param distribution Description
+#'
+#' @return what it returns
+#'
+#' @export
+#'
 PPCC <- function(data, distribution) {
+
+  U_i <- function(ordered_data){
+    n <- length(ordered_data)
+    m_i <- rep(NA, n)
+    m_i[n] <- 0.5**(1/n)
+    m_i[1] <- 1 - m_i[n]
+    for (i in 2:n-1){
+      m_i[i] <- (i - 0.3175)/(n + 0.365)
+    }
+    m_i
+  }
+
+  tukey_order <- function(mi, lambda){
+    (mi**lambda - (1-mi)**lambda) / lambda
+  }
+
   data <- sort(data)
   xval <- c()
   yval <- c()
@@ -30,6 +36,9 @@ PPCC <- function(data, distribution) {
   plot(xval, yval)
 }
 
+# The Tukey-Lambda PPCC plot is used to suggest an appropriate distribution.
+# You should follow-up with PPCC and probability plots of the appropriate alternatives.
+# https://www.itl.nist.gov/div898/handbook/eda/section3/ppccplot.htm
 
 # To make the probability plot we use:
 # https://www.itl.nist.gov/div898/handbook/eda/section3/probplot.htm
