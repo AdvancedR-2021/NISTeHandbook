44e307dbc25366efbb8d6f0b1781c2007252bb7a
Tony Lung Ca Hin <tonyhin48@gmail.com>
2021-11-08 17:55:30 +0100

    Alpha version of Tietjen - Moore test. htest removed. Made a custom "htest" object.

--- R/TMTest.r
+++ R/TMTest.r
@@ -1,21 +1,19 @@
-library(tidyverse)
-
-
-## Input data.
-x = c(-1.40, -0.44, -0.30, -0.24, -0.22, -0.13, -0.05,
-      0.06, 0.10, 0.18, 0.20, 0.39, 0.48, 0.63, 1.01)
-
-## Specify k, the number of outliers being tested.
-k = 3
-
-## Generate normal probability plot.
-qqnorm(x)
-
-boxplot(x)
-
-## Create a function to compute statistic to
-## test for outliers in both tails.
-TMTest = function(x,k){
+#' Function to test for k outliers in both tails.
+#'
+#' @param x univariate data
+#' @param k number of suspected outliers
+#'
+#' @return An object that shows results of hypothesis
+#' @export
+#'
+#' @examples
+#' x = c(-1.40, -0.44, -0.30, -0.24, -0.22, -0.13, -0.05,
+#' 0.06, 0.10, 0.18, 0.20, 0.39, 0.48, 0.63, 1.01)
+#' Specify k, the number of outliers being tested.
+#' k = 3
+#' TMTest = function(x,k)
+#'
+TMTest <- function(x,k){
     tm <- function(x,k){
       n = length(x)
 
@@ -53,28 +51,24 @@ TMTest = function(x,k){
 
   lst <- list(ek = ekstat,
               h0 = "there are no outliers in the data",
-              ha = cat("the",k, "most extreme points are outliers"),
+              ha = c("the ",k, " most extreme points are outliers"),
               ek = ekstat,
               method = "Tietjen Moore Test",
               alpha = 0.05,
-              low_tail = cat("Critical value for lower tail ", ekstat),
-              region = cat("Critical region: Reject H0 if E_k < ", quantile(test,0.05))
-              )
+              low_tail =  quantile(test,0.05))
   class(lst) <- "tmtest"
-  print(lst)
-
+  lst
 }
 
 print.tmtest <- function(x, ...){
-  print(x$h0)
-  print(x$ha)
-  print(x$ek)
-  print(x$method)
-  print(x$alpha)
-  print(x$low_tail)
-  print(x$region)
+  cat("\nResults of ", x$method, "\n")
+  cat("--------------------------\n")
+  cat("H_0:  ", x$h0, "\n")
+  cat("H_a:  ", x$ha, "\n\n")
+  cat("Test statistic:  E_k = ", x$ek, "\n")
+  cat("Significance level:  a = ", x$alpha, " \n")
+  cat("Critical value for lower tail:  ", x$low_tail, " \n")
+  cat("Critical region: Reject H_0 if E_k < ", x$low_tail, " \n")
   invisible(x)
 }
-
-TMTest(x,2)
 
